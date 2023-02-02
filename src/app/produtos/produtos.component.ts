61dfc53a1187d0000bfffe229338e704d518a545
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2023-02-02 15:29:58 -0300

    Implementando barra de pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduto, produtos } from '../produtos';
 import { ProdutosService } from '../produtos.service';
 
@@ -12,10 +13,22 @@ export class ProdutosComponent {
   produtos: IProduto[] | undefined;
 
   constructor(
-    private produtosService: ProdutosService
+    private produtosService: ProdutosService,
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.produtos = this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+
+    this.activatedRoute.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLocaleLowerCase();
+      
+      if (descricao) {
+        this.produtos = produtos.filter(produto => produto.descricao.toLocaleLowerCase().includes(descricao));
+        return;
+      }
+
+      this.produtos = produtos;
+    })
   }
 }
