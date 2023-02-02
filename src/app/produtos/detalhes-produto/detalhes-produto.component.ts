21b54824c05792c927c24da9a6de80f6e4a980f9
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2023-02-02 10:30:25 -0300

    Serviço de produtos

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-detalhes-produto',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class DetalhesProdutoComponent {
 
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtosService: ProdutosService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void{
+    const routeParams = this.activatedRoute.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
 }
